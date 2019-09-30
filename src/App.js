e31938b9632408dec501329aaa1ae4508c54fc0f
MUGABA <mugabamuha@gmail.com>
2019-09-30 14:09:09 +0300

    just added our header componet and add our svg icon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from './pages/homePage/homePageComponent'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shopComponent'
+import Header from './components/header/headerComponent'
 import './App.css';
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path ='/' component = {HomePage} />
         <Route path ='/shop' component = {ShopPage} />
