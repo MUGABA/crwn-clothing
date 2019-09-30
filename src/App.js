12b8c32b0479c13d2548297f5443effa39c53ad3
MUGABA <mugabamuha@gmail.com>
2019-09-30 10:36:50 +0300

    just added more components and structred my project well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homePageComponent'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+
+const HatPage = () =>(
+  <div>
+    <h1>This is the hats page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route exact path ='/' component = {HomePage} />
+        <Route path ='/shop/hats' component = {HatPage} />
+      </Switch>
     </div>
   );
 }
